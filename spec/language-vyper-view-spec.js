b69be00ffb358850b8e70f35363d79112479ced0
x <x@x.x>
2019-09-23 15:20:33 +0200

    first commit

---
+++ spec/language-vyper-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageVyperView from '../lib/language-vyper-view';
+
+describe('LanguageVyperView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
